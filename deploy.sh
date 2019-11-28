dae686fbf8c652eed436d4ea044c553776b93f19
Yi-Lin Juang <frankyjuang@gmail.com>
2019-11-29 06:57:24 +0800

    Serve AASA as json

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 DISTRIBUTION_ID=${1}
+BUCKET="web.production.milkjobs.ga"
+BUILD_DIR="build/"
 
 echo "Deploying..."
-aws s3 sync build/ s3://web.production.milkjobs.ga --region ap-northeast-1 --delete
+aws s3 sync ${BUILD_DIR} s3://${BUCKET} --region ap-northeast-1 --delete
+# Set content type of AASA to json
+# https://developer.apple.com/library/archive/documentation/General/Conceptual/AppSearch/UniversalLinks.html
+aws s3 cp ${BUILD_DIR}.well-known/apple-app-site-association s3://${BUCKET}/.well-known/ --content-type application/json
 echo "Invalidating..."
 aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths /index.html
 echo "Deployment complete!!!"
