104b1c9f39d2a5273166a937168d0e99b09d6e8b
darrenyaoyao <g789872001darren@gmail.com>
2019-12-29 01:28:45 +0800

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ const AppRouter: React.FC<{}> = () => {
   return (
     <div className="App">
       <PageMetadata
-        title="牛奶找工作：工作，是為了更好的生活"
-        description="牛奶找工作 App 提供最快速、直接的求職求才服務。求職者與公司直接聊、加快面試、即時反饋，隨時隨地都能掌握人才的訊息。"
+        title="牛奶找工作，大學生的求職平台：工作，是為了更好的生活"
+        description="牛奶找工作，專注於大學生、新鮮人的求職機會。我們的 App 提供最快速、直接的求職求才服務。求職者與公司直接聊、加快面試、即時反饋，隨時隨地都能掌握人才的訊息。"
         image={logo}
       />
       <BrowserRouter>
