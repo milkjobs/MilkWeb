f839402dd34c457db8f2fa37990afa51da754ad5
Yi-Lin Juang <frankyjuang@gmail.com>
2019-12-29 23:45:51 +0800

    Block search index for staging

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import logo from "assets/milk.png";
+import { environment } from "config";
 import { ErrorCatcher, PageMetadata } from "helpers";
 import React from "react";
-import { HelmetProvider } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { BrowserRouter, Route, RouteProps, Switch } from "react-router-dom";
 import PageView from "routes/PageView";
 import RecruiterRoute from "routes/RecruiterRoute";
 import RootRoute from "routes/RootRoute";
 import { AuthProvider, ThemeProvider, useAuth } from "stores";
-import "./App.css";
 import { ChannelProvider } from "stores/channel";
+import "./App.css";
 
 const LoadingRoute: React.FC<RouteProps> = props => {
   const { loading } = useAuth();
@@ -20,6 +21,12 @@ const LoadingRoute: React.FC<RouteProps> = props => {
 const AppRouter: React.FC<{}> = () => {
   return (
     <div className="App">
+      {environment !== "production" && (
+        // https://support.google.com/webmasters/answer/93710
+        <Helmet>
+          <meta name="robots" content="noindex" />
+        </Helmet>
+      )}
       <PageMetadata
         title="牛奶找工作，大學生的求職平台：工作，是為了更好的生活"
         description="牛奶找工作，專注於大學生、新鮮人的求職機會。我們的 App 提供最快速、直接的求職求才服務。求職者與公司直接聊、加快面試、即時反饋，隨時隨地都能掌握人才的訊息。"
