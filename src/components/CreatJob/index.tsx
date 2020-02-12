1754104e2e4cd0a9d53461f33f98dfb76d1ee450
darrenyaoyao <g789872001darren@gmail.com>
2020-02-12 20:15:56 +0800

    Remove legacy code

--- src/components/CreatJob/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as CreateFooter } from "./createJobFooter";
-export { default as CreateJobDescription } from "./createJobDescription";
-export { default as CreateJobInfo } from "./createJobInfo";
-export { default as CreateJobTitle } from "./createJobTitle";
--- src/components/CreateTeam/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as CreateTeamInfo } from "./createTeamInfo";
-export { default as CreateTeamIntroduction } from "./createTeamIntroduction";
-export { default as CreateTeamMoreInfo } from "./createTeamMoreInfo";
-export { default as CreateTeamName } from "./createTeamName";
