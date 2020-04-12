7231d36fe3db38d2b6e5d04f383548e1839c8591
Yi-Lin Juang <frankyjuang@gmail.com>
2020-04-13 06:16:22 +0800

    Fix ssr

---
+++ server/README.md
@@ -0,0 +1,20 @@
+# Server Side Rendering
+
+## Installation
+
+```shell
+yarn
+```
+
+## Local Development
+
+```shell
+yarn start
+yarn invoke:local
+```
+
+## Deployment
+
+```shell
+yarn deploy:production
+```
--- server/lambda.ts
+++ server/lambda.ts
@@ -1,19 +1,9 @@
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  Handler
-} from "aws-lambda";
 import awsServerlessExpress from "aws-serverless-express";
 import app from "./app";
 
 const server = awsServerlessExpress.createServer(app);
 
-const main: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = (
-  event,
-  context,
-  callback
-) => {
-  awsServerlessExpress.proxy(server, event, context, "CALLBACK", callback);
-};
+const main = (event, context) =>
+  awsServerlessExpress.proxy(server, event, context, "PROMISE").promise;
 
 export { main };
